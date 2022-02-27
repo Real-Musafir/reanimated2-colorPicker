e6550ab4e1c2dbb00b7a1b5fea10c661a802e459
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-02-27 23:24:01 +0600

    create circle picker4 in gradient

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -1,11 +1,26 @@
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 function ColorPicker({ colors, start, end, style }) {
   return (
-    <LinearGradient colors={colors} start={start} end={end} style={style} />
+    <View style={{ justifyContent: "center" }}>
+      <LinearGradient colors={colors} start={start} end={end} style={style} />
+      <View style={styles.picker} />
+    </View>
   );
 }
+
+const CIRCLE_PICKER_SIZE = 45;
+
+const styles = StyleSheet.create({
+  picker: {
+    position: "absolute",
+    backgroundColor: "white",
+    width: CIRCLE_PICKER_SIZE,
+    height: CIRCLE_PICKER_SIZE,
+    borderRadius: CIRCLE_PICKER_SIZE / 2,
+  },
+});
 
 export default ColorPicker;
